e54b39f32fc3d932c2f5952210191d667468a5ad
marcu <marcuswahlstedt19@gmail.com>
2020-05-06 13:14:42 +0200

    finished

--- jquery.js
+++ jquery.js
@@ -1,16 +1,7 @@
 $(document).ready(function(){
-	$("#hideButton").click(function(){
-		$("form").slideToggle();
-	});	
-	$("#fade").click(function(){
-		$("h3").fadeToggle();
-	});
-
-	$("#animation").click(function(){
-		$("div").animate({
-    left: '250px',
-    height: '+=150px',
-    width: '+=150px'
-	});
-	});
+    $(".hover").hover(function(){
+        $(this).css("color", "darkred");
+    }, function(){
+        $(this).css("color", "black");
+    });
 });
